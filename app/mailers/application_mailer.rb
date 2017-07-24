faa7f77c540500f39ef1a31e8f4ed29c7d81ffaf
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-24 15:22:25 -0700

    update mailer template to add logo attachment. update email template footers.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+  private
+  def add_inline_attachment!
+    attachments.inline['textlogo.png'] = File.read(File.join(Rails.root,'app','assets','images','textlogo.png'))
+  end
 end
