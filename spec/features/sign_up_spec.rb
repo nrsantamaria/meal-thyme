82376ed301184675d1a447151fda10f82235e328
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-24 11:45:33 -0700

    update sign up spec. pass spec.

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'the user sign up process' do
   it 'adds a user to the system' do
+    visit '/users/sign_up'
     user = FactoryGirl.create(:user)
-    visit '/'
     fill_in 'user[username]', :with => user.username
-    fill_in 'user[email]', :with => user.email
+    fill_in 'user[email]', :with => 'email2@email.com'
     fill_in 'user[password]', :with => user.password
-    fill_in 'Password confirmation', :with => user.password
+    fill_in 'user_password_confirmation', :with => user.password
     click_on 'Sign up'
     expect(page).to have_content 'Profile Page'
   end
