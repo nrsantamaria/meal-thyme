d383598e1bc1bb631c018c732e9f62da0ec19a96
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-25 11:53:33 -0700

    add json file and update seed file. seed method will not work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.write_json
+  end
+
+  def write_json
+    recipePath = "#{Rails.root}/public/recipes7.25.v1.json"
+    recipes = JSON.parse(File.read(recipePath))
+    recipes.each do |recipe|
+      
+      puts(recipe['title'])
+      # puts(recipe[1][0]['title'])
+    end
+  end
+end
+
+Seed.begin
