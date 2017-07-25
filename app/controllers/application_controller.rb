55c45dbc1b4caf87b31757364f8fccbc06b19464
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-25 10:03:12 -0700

    add current user helper method to application controller. update application html with user profile and sign out links. update users controller with authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   private
   def after_sign_out_path_for(resource_or_scope)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+  before_action :only => [:show] do
+    flash.alert = "You must be logged in to access that page."
+    redirect_to '/' unless current_user
+  end
+
   def index
     render 'home#index'
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
