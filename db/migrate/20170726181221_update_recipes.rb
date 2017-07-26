521b20bd90f14ca89fa8849edb2a85dd6723bbf7
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-26 11:18:01 -0700

    add migration to update recipes table to add relationship to meals.

---
+++ db/migrate/20170726181221_update_recipes.rb
@@ -0,0 +1,5 @@
+class UpdateRecipes < ActiveRecord::Migration[5.1]
+  def change
+    add_column(:recipes, :meal_id, :integer)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726175411) do
+ActiveRecord::Schema.define(version: 20170726181221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170726175411) do
     t.integer "fat"
     t.integer "carbohydrates"
     t.text "instructions", default: [], array: true
+    t.integer "meal_id"
   end
 
   create_table "users", force: :cascade do |t|
