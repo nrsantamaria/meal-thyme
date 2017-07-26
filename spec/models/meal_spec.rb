b81308dc32a1fc0098fce0b2962fb9cefacbca71
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-26 13:11:47 -0700

    add and pass spec for relationship for meal to recipe

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 describe Meal do
-  it { should have_many :recipes }
+  
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 describe Recipe do
-  it { should belong_to :meals }
+  it { should have_one :meal }
 end
