c75015b2ee2fdec7065d18007368a97ec283fc56
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-09-25 13:55:34 -0700

    add to recipes spec.

--- spec/features/get_recipes_spec.rb
+++ spec/features/get_recipes_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe 'get all the recipes' do
   it 'shows all the recipes in the database' do
+    visit '/'
+    user = FactoryGirl.create(:user)
+    fill_in 'user[username]', :with => user.username
+    fill_in 'user[email]', :with => user.email
+    fill_in 'user[password]', :with => user.password
+    click_on 'Log in'
     visit recipes_path
     expect(page).to have_content 'Recipes'
   end
